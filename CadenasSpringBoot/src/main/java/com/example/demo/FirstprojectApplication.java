fe205cbc2aa5fce26d07330ffb4eeb0ecb6b9594
github_teresaMorales <teresa.amh21@gmail.com>
2021-01-29 20:40:15 -0300

    se sube actividad de cadenas con Spring Boot

---
+++ CadenasSpringBoot/src/main/java/com/example/demo/FirstprojectApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class FirstprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectApplication.class, args);
+		
+	}
+	@RequestMapping("/")
+	public String ochenta() {
+		return "Hello client! How are you doing?";
+	}
+	@RequestMapping("/random")
+	public String random() {
+		return "Spring Boot is great! So easy to just respond with strings";
+	}
+
+}
+
